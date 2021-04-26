ce2c26c03a0ff0bf8b828881650b632f2f84068d
Jonathan Trans <79351500+Jonathantrans@users.noreply.github.com>
2021-04-25 17:55:40 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -108,6 +108,12 @@ window.addEventListener('scroll', () => {
     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
 })  
        
+var card = $(".card");
 
+$(document).on("mousemove",function(e) {  
+  var ax = -($(window).innerWidth()/2- e.pageX)/20;
+  var ay = ($(window).innerHeight()/2- e.pageY)/10;
+  card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+});
   })(jQuery); // End of use strict
   
