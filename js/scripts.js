79b74829ec13f538f8d76a61fc0ca28ae0443376
Jonathan Trans <79351500+Jonathantrans@users.noreply.github.com>
2021-05-27 00:39:09 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
     */
 
-tippy('button')
+tippy('button btn-1')
 
     (function($) {
     "use strict"; // Start of use strict
